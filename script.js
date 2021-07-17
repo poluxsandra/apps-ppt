032b0c9b7f35a6c6f39c0ae2b44fb98933419d10
Sandra Martini <sandrinhaamartini@gmail.com>
2021-07-17 00:21:38 -0300

    style: indentation problems
    * applied indentation pattern (personal pattern)
         - HTML/CSS: 4 spaces;
         - JavaScript: 2 spaces.

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let robot = "Guth"
 let bag = ["Pedra", "Papel", "Tesoura"]
 
 let item = Array.prototype.sample = function(){
-  return this[Math.floor(Math.random()*this.length)];
+    return this[Math.floor(Math.random()*this.length)];
 } 
 
 let player2 = bag.sample();
@@ -28,45 +28,45 @@ let player2 = bag.sample();
 //Pedra
 
 function rock () {
-      if (player2 == "Pedra") {
+    if (player2 == "Pedra") {
         document.write("Empate. Você é bom hein..")
-      }
-      else if (player2 == "Papel") {
+}
+    else if (player2 == "Papel") {
         document.write("Papel envolve a Pedra. O vencedor é " + robot + "É.. Parece que eu venci hehe")
-      }
-      else if (player2 == "Tesoura") {
+}
+    else if (player2 == "Tesoura") {
         document.write("Pedra quebra a Tesoura. O vencedor é " + name + ". Parabéns!!!")
-      }
+}
 }
 
 
 //Tesoura
 
 function scis () {
-      if (player2 == "Pedra") {
+    if (player2 == "Pedra") {
         document.write("Pedra quebra a Tesoura. O vencedor é " + robot + ". É.. Parece que eu venci hehe")
-      }
-      else if (player2 == "Papel") {
+}
+    else if (player2 == "Papel") {
         document.write("Tesoura corta o Papel. O vencedor é " + name + ". Parabéns!!!")
-      }
-      else if (player2 == "Tesoura") {
+}
+    else if (player2 == "Tesoura") {
         document.write("Empate. Você é bom hein..")
-      }
+}
 }
 
 
 //Papel
 
 function paper () {
-      if (player2 == "Pedra") {
+    if (player2 == "Pedra") {
         document.write("Papel envolve a Pedra. O vencedor é " + name + ". Parabéns!!!")
-      }
-      else if (player2 == "Papel") {
+    }
+    else if (player2 == "Papel") {
         document.write("Empate. Você é bom hein..")
-      }
-      else if (player2 == "Tesoura") {
+}
+    else if (player2 == "Tesoura") {
         document.write("Tesoura corta o Papel. O vencedor é " + robot + ". É.. Parece que eu venci hehe")
-      }
+}
 }
 
 
@@ -77,9 +77,9 @@ let tesoura = document.querySelector('#scis2');
 let papel = document.querySelector('#paper2')
 
 function load() {
-pedra.addEventListener("click", rock, false)
-tesoura.addEventListener("click", scis, false)
-papel.addEventListener("click", paper, false)
+    pedra.addEventListener("click", rock, false)
+    tesoura.addEventListener("click", scis, false)
+    papel.addEventListener("click", paper, false)
 }
 
 document.addEventListener("DOMContentLoaded", load, false);
