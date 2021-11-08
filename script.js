902b3b8288ff0e1100cc43c9246b5dd754fb5e37
Sandra Martini <sandrinhaamartini@gmail.com>
2021-11-08 11:58:53 -0300

    feat(script): contruído os métodos para o funcionamento do Guth (IA)

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ let name = prompt("Qual seu nome")
 //Prólogo
 
 alert("Entendi. Bem vindo(a) " + name + ". Vamos começar!! É só selecionar o item na próxima tela")
+
+
+//Guth
+
+let robot = "Guth"
+let bag = ["Pedra", "Papel", "Tesoura"]
+
+let item = Array.prototype.sample = function(){
+  return this[Math.floor(Math.random()*this.length)];
+} 
+
+let player2 = bag.sample();
