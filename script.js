9d2df31ba4f460dabdf23184afaa559bf2bd88ff
Sandra Martini <sandrinhaamartini@gmail.com>
2021-11-10 13:53:08 -0300

    fix(regras de negócio): corrigido a pontuação da segunda caixa de diálogo
    * adicionado ponto de interrogação do alerta que recebe o nome do jogador.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ alert("Oi, me chamo Guth, vamos jogar Pedra, Papel e Tesoura, mas primeiro..")
 
 //Dados do Jogador
 
-let name = prompt("Qual seu nome")
+let name = prompt("Qual seu nome?")
 
 
 //Prólogo
